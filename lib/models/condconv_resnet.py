c6fa172a0e313bf9e0b040edb8cd573bd4f647b8
haomiao <haomiao@haomiaodeMacBook-Pro.local>
2021-01-17 11:52:15 +0800

    update

--- lib/models/condconv_resnet.py
+++ lib/models/condconv_resnet.py
@@ -126,6 +126,7 @@ class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=1000, zero_init_residual=False, num_experts=1):
         super(ResNet, self).__init__()
+        assert num_experts == 8
         self.inplanes = 64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
                                bias=False)
